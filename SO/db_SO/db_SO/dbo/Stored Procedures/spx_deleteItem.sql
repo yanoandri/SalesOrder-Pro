07a0679df717a165c5ce0c8f0d752d4d07b9a213
YanoAndri <yano.andriyanto@profescipta.co.id>
2015-04-16 18:33:04 +0700

    struktur db terbaru

---
+++ SO/db_SO/db_SO/dbo/Stored Procedures/spx_deleteItem.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE spx_deleteItem
+	@ITEMID AS INT,
+	@SOID AS INT
+AS
+BEGIN
+	DELETE dbo.SALES_SO_LITEM
+	WHERE SALES_SO_LITEM_ID = @ITEMID AND SALES_SO_ID = @SOID
+END
