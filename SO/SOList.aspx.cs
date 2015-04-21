dcf4c1b995b1b2b31f0f7559a76c413a47afb5e6
YanoAndri <yano.andriyanto@profescipta.co.id>
2015-04-21 09:52:23 +0700

    Enhance Find List

--- SO/SOList.aspx.cs
+++ SO/SOList.aspx.cs
@@ -28,9 +28,9 @@ namespace SO
 
         protected void btnsearch_Click(object sender, EventArgs e)
         {
-           
+
             FindList();
-         
+
         }
 
         protected void Button3_Click(object sender, EventArgs e)
@@ -40,9 +40,9 @@ namespace SO
 
         protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
         {
-            
+
             Session["Edit"] = Convert.ToInt32(e.CommandArgument.ToString());
-            
+
             if (e.CommandName == "Edit")
             {
                 Response.Redirect("SOInput.aspx?SOID=" + Session["Edit"]);
@@ -97,7 +97,11 @@ namespace SO
             }
             else if (txtCalendar.Text != "")
             {
-                cmd = "SELECT SALES_SO_ID, a.SO_NO, a.ORDER_DATE, b.CUSTOMER_NAME, a.ADDRESS FROM dbo.SALES_SO a JOIN dbo.COM_CUSTOMER b ON a.COM_CUSTOMER_ID = b.COM_CUSTOMER_ID WHERE (a.SO_NO LIKE '%" + txtkey.Text + "%' OR b.CUSTOMER_NAME LIKE '%" + txtkey.Text + "%' AND ORDER_DATE= '" + txtCalendar.Text + "')";
+                cmd = "SELECT SALES_SO_ID, a.SO_NO, a.ORDER_DATE, b.CUSTOMER_NAME, a.ADDRESS FROM dbo.SALES_SO a JOIN dbo.COM_CUSTOMER b ON a.COM_CUSTOMER_ID = b.COM_CUSTOMER_ID WHERE ORDER_DATE= '" + txtCalendar.Text + "'";
+            }
+            else
+            {
+                cmd = "SELECT SALES_SO_ID, a.SO_NO, a.ORDER_DATE, b.CUSTOMER_NAME, a.ADDRESS FROM dbo.SALES_SO a JOIN dbo.COM_CUSTOMER b ON a.COM_CUSTOMER_ID = b.COM_CUSTOMER_ID WHERE (a.SO_NO LIKE '%" + txtkey.Text + "%' OR b.CUSTOMER_NAME LIKE '%" + txtkey.Text + "%') AND ORDER_DATE= '" + txtCalendar.Text + "'";
             }
             SqlCommand sqlcmd = new SqlCommand(cmd, cn);
             sqlcmd.CommandType = CommandType.Text;
